348883e467e896cde50358a3ac78ea844e000785
yu-xiaomeng <414017727@qq.com>
2021-11-04 18:45:07 +0800

    [feat: #5] use a uniform return format for response

---
+++ src/main/java/com/xiaomeng/dailycost/base/ResultData.java
@@ -0,0 +1,59 @@
+package com.xiaomeng.dailycost.base;
+
+public class ResultData<T> {
+    private int code;
+    private String message;
+    private T data;
+    private long timestamp ;
+
+    public ResultData() {
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public static <T> ResultData<T> success(T data) {
+        ResultData<T> resultData = new ResultData<>();
+        resultData.setCode(ReturnCode.RC100.getCode());
+        resultData.setMessage(ReturnCode.RC100.getMessage());
+        resultData.setData(data);
+        return resultData;
+    }
+
+    public static <T> ResultData<T> fail(int code, String message) {
+        ResultData<T> resultData = new ResultData<>();
+        resultData.setCode(code);
+        resultData.setMessage(message);
+        return resultData;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+}
---
+++ src/main/java/com/xiaomeng/dailycost/base/ReturnCode.java
@@ -0,0 +1,32 @@
+package com.xiaomeng.dailycost.base;
+
+public enum ReturnCode {
+    /**操作成功**/
+    RC100(100,"操作成功"),
+    /**ID不存在**/
+    RC4001(200,"id is not exist"),
+    /**操作失败**/
+    RC999(999,"操作失败"),
+    /**服务异常**/
+    RC500(500,"系统异常，请稍后重试");
+
+
+
+    /**自定义状态码**/
+    private final int code;
+    /**自定义描述**/
+    private final String message;
+
+    ReturnCode(int code, String message){
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
